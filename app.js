42fc7e8438f104e48c156804cb62287b5d3cbc40
Nick Kessel <80365598+nickkessel@users.noreply.github.com>
2022-02-02 15:56:10 -0500

    added basic js

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
-//nothing here yet...
+const todoObjectList = [];
+
+class Todo_Class {
+  constructor(item){
+    this.ulElement =item;
+  }
+
+  add() {
+    const todoInput = document.querySelector("#myInput").value;
+    if(todoInput == "") {
+      alert("wheres the item?")
+    } else {
+      const todoObject = {
+        id: todoObjectList.length,
+        todoText : todoInput,
+        isDone : false,
+      }
+
+      todoObjectList.unshift(todoObject);
+      this.display()
+      document.querySelector("#myInput").value = '';
+    }
+  }
+
+  done_undone(x){
+
+  }
+
+  deleteElement(x){
+
+  }
+
+  display(){
+
+  }
+}
+
+const listSection = document.querySelector("#myUL");
+
+myTodoList = new Todo_Class(listSection);
+
+document.querySelector(".addBtn").addEventListener("click", function() {
+  myTodoList.add()
+})
